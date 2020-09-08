5e03a9eaba0bdbc6605852cfe826acaeca4e6408
Eric Folenta <ericfolenta@gmail.com>
2020-09-07 20:43:17 -0700

    updated sql files for reliable entry into database

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(50),
-  devoured BOOLEAN NOT NULL DEFAULT false,
+  devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Double Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Baja Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Carolina Reaper Burger");
