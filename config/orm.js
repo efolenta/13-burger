2763c3a3714dbf44ea2ab5cf0fa0cf0fd99e7c7f
Eric Folenta <ericfolenta@gmail.com>
2020-09-20 13:34:57 -0700

    added orm framework

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,26 @@
 var connection = require("./connection.js");
 
-console.log("Connected to:", connection.config.database);
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+var orm = {
+    selectAll: function() {
+        connection.query("SELECT * FROM burgers", function() {
+            if (err) throw err;
+        })
+    },
+
+    insertOne: function() {
+        connection.query("INSET INTO burgers SET ?")
+        
+    },
+
+    updateOne: function() {
+    
+    }
+};
+
+module.exports = orm;
